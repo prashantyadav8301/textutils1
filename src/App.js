3414755379b26e3a6b41b92366fd6b443529b01c
unknown <prashantyadav5545@gmail.com>
2023-10-15 22:56:02 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
